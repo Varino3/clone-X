ccf3db20383c14fb1e50e6441b46e2e0617a6f75
Alvaro Contreras <contreras1999@gmail.com>
2023-11-15 10:21:25 +0100

    nuevas funcionalidades Tweet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,35 +6,44 @@ import UserProfile from '../components/UserProfile';
 import { addTweet } from '../store/actions';
 
 const Home = () => {
+    const user = useSelector((state) => state.user);
     const tweets = useSelector((state) => state.tweets);
     const dispatch = useDispatch();
 
     const [newTweetText, setNewTweetText] = useState('');
 
     const handleAddTweet = () => {
-        if (newTweetText.trim() !== '') {
-            const newTweet = {
-                id: Date.now(),
-                text: newTweetText,
-            };
-            dispatch(addTweet(newTweet));
-            setNewTweetText('');
+        if (user) {
+            // Verificar si el usuario ha iniciado sesión antes de agregar un tweet
+            if (newTweetText.trim() !== '') {
+                const newTweet = {
+                    id: Date.now(),
+                    text: newTweetText,
+                };
+                dispatch(addTweet(newTweet));
+                setNewTweetText('');
+            }
+        } else {
+            // Mostrar un mensaje o redirigir a la página de inicio de sesión
+            console.log('Debes iniciar sesión para escribir un tweet.');
         }
     };
 
     return (
         <div className='home-container'>
             <UserProfile />
-            <h2>Inicio</h2>
-            <div className='tweet-form'>
-                <textarea
-                    rows="3"
-                    placeholder="Escribe un nuevo tweet"
-                    value={newTweetText}
-                    onChange={(e) => setNewTweetText(e.target.value)}
-                />
-                <button onClick={handleAddTweet}>Tweetear</button>
-            </div>
+            {user && (
+                <div className='tweet-form'>
+                    <h2>Inicio</h2>
+                    <textarea
+                        rows="3"
+                        placeholder="Escribe un nuevo tweet"
+                        value={newTweetText}
+                        onChange={(e) => setNewTweetText(e.target.value)}
+                    />
+                    <button onClick={handleAddTweet}>Tweetear</button>
+                </div>
+            )}
             <TweetList tweets={tweets} />
         </div>
     );
