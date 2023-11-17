3d8e99edcfca3d9adac7894ec4fc59da0880a161
Alvaro Contreras <contreras1999@gmail.com>
2023-11-17 11:12:16 +0100

    filtro - buscador

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,28 +1,17 @@
-// Tweet.jsx
 import React from 'react';
-import { deleteTweet } from './indexedDB';
 
 const Tweet = ({ tweet, users, onDelete }) => {
   const userId = tweet.userId;
   const user = users.find((u) => u.uuid === userId) || {};
 
-  const handleDelete = async () => {
-    try {
-      await deleteTweet(tweet.id);
-      onDelete();
-    } catch (error) {
-      console.error('Error al eliminar el tweet:', error);
-    }
-  };
-
   return (
     <div className='tweet'>
       <div className='tweet-user'>
-        Usuario: {user.nombre_usuario}
+        Usuario: {tweet.nombre_usuario}
       </div>
       <div className='tweet-text'>{tweet.text}</div>
       {onDelete && (
-        <button onClick={handleDelete}>Eliminar</button>
+        <button onClick={() => onDelete(tweet.id)}>Eliminar</button>
       )}
     </div>
   );
